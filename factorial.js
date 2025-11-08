f321f394a7aba9ba951a7db0983cb0bc00a26a16
Student <student@example.com>
2025-11-08 17:51:03 +0000

    Assignment factorials

--- factorial.js
+++ factorial.js
@@ -1,5 +1,20 @@
 function factorial(num) {
-  return 0;
+  if (num === 0) {
+    return 1;
+  }
+  let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+    result *=i;
+  }
+
+  return result;
 }
+
+console.log(factorial(5)); // 120
+console.log(factorial(3)); // 6
+console.log(factorial(0)); // 1
+
+
 
 module.exports = factorial;
